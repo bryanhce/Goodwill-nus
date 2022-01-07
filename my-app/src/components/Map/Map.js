fa1513d9dc2ee639508c1805a9b26556b0ee472d
Navaneeth R <nramapurath@gmail.com>
2022-01-07 12:57:02 +0800

    removed gmaps api key

--- my-app/src/components/Map/Map.js
+++ my-app/src/components/Map/Map.js
@@ -50,7 +50,7 @@ const CompleteMap = (props) => {
   return (
     <div className="completeMap__border">
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBfjRrLpo0VPNQMI_CEtUmfpvXyaM3L58M`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
